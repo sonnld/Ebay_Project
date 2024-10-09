0492ae9b6d2718dc8412c12b721f3b33079a6997
anhnguyet <anhnguyet1303ttg@gmail.com>
2024-10-09 15:01:35 +0700

    fix connect DB

--- Ebay_Project_PRN/Model/EBay_ProjectContext.cs
+++ Ebay_Project_PRN/Model/EBay_ProjectContext.cs
@@ -43,7 +43,7 @@ namespace Ebay_Project_PRN.Model
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server =DESKTOP-NBUV2GE\\DAISON; database = EBay_Project;uid=sa;pwd=sa;");
+                optionsBuilder.UseSqlServer("server =MSI; database = EBay_Project;uid=sa;pwd=123456;");
             }
         }
 
